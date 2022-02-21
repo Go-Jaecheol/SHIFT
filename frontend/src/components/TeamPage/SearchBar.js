bd269de1822a6f8a4c5a3541fc48ec25c8ae2b0c
Go-Jaecheol <gojaech@naver.com>
2022-02-21 15:26:02 +0900

    Add SearchBar textfield

--- frontend/src/components/TeamPage/SearchBar.js
+++ frontend/src/components/TeamPage/SearchBar.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from 'styled-components';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
 
 const SearchBarWrapper = styled.div`
     height: 10%;
@@ -11,8 +13,21 @@ const SearchBarWrapper = styled.div`
 const SearchBar = () => {
     return (
         <SearchBarWrapper>
+            <Autocomplete
+                disablePortal
+                id="seat-section"
+                options={sectionList}
+                sx={{ width: 200 }}
+                renderInput={(params) => <TextField {...params} label="구역" />}
+            />
+            <TextField id="seat-col" label="열" variant="outlined" />
+            <TextField id="seat-num" label="번호" variant="outlined" />
         </SearchBarWrapper>
     );
 }
+
+const sectionList = [
+    { label: '블루존 3-2' },
+]
 
 export default SearchBar;
