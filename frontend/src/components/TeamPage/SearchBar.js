25bf8a5f8dc7413285af7c3de714f75c12a8e869
Go-Jaecheol <gojaech@naver.com>
2022-02-23 02:35:17 +0900

    Change SearchBar design & Add SearchResult content

--- frontend/src/components/TeamPage/SearchBar.js
+++ frontend/src/components/TeamPage/SearchBar.js
@@ -7,21 +7,50 @@ const SearchBarWrapper = styled.div`
     height: 10%;
     width: 1100px;
     border-radius: 5px;
-    background-color: rgba(246,246,246,0.5);
+    background-color: rgba(230,230,230,0.5);
+    display: flex;
+    justify-content: center;
+`;
+
+const SearchBarContent = styled.div`
+    height: 70px;
+    width: 900px;
+    padding-top: 15px;
+    margin-top: 10px;
+    border-radius: 25px;
+    background-color: #FFFFFF;
+    display: flex;
+    justify-content: center;
+`;
+
+const SearchBtn = styled.button`
+    height: 56px;
+    width: 56px;
+    margin-left: 5px;
+    border-radius: 5px;
+    background-color: #5090d3;
+    border: 0;
+    cursor: pointer;
+    &:hover {
+        border: 2px solid white;
+    }
 `;
 
 const SearchBar = () => {
     return (
         <SearchBarWrapper>
-            <Autocomplete
-                disablePortal
-                id="seat-section"
-                options={sectionList}
-                sx={{ width: 200 }}
-                renderInput={(params) => <TextField {...params} label="구역" />}
-            />
-            <TextField id="seat-col" label="열" variant="outlined" />
-            <TextField id="seat-num" label="번호" variant="outlined" />
+            <SearchBarContent>
+                <Autocomplete
+                    disablePortal
+                    id="seat-section"
+                    options={sectionList}
+                    sx={{ width: 200 }}
+                    renderInput={(params) => <TextField {...params} label="구역" />}
+                />
+                <TextField id="seat-col" label="열" variant="outlined" />
+                <TextField id="seat-num" label="번호" variant="outlined" />
+                <SearchBtn>Search</SearchBtn>
+            </SearchBarContent>
         </SearchBarWrapper>
     );
 }
--- frontend/src/components/TeamPage/SearchResult.js
+++ frontend/src/components/TeamPage/SearchResult.js
@@ -1,19 +1,92 @@
 import React from "react";
 import styled from 'styled-components';
+import Rating from '@mui/material/Rating';
 
 const SearchResultWrapper = styled.div`
     display: flex;
-    flex-direction: column;
-    margin-top: 10px;
+    justify-content: center;
     height: 80%;
     width: 1100px;
     border-radius: 5px;
-    background-color: rgba(246,246,246,0.5);
+    background-color: rgba(230,230,230,0.5);
+`;
+
+const SearchResultContent = styled.div`
+    display: flex;
+    flex-direction: column;
+    background-color: #FFFFFF;
+    height: 90%;
+    width: 1000px;
+    margin-top: 30px;
+    border-radius: 5px;
+`;
+
+const SearchResultInfo = styled.div`
+    display: flex;
+    align-items: center;
+    background-color: rgba(240,240,240,0.5);
+    height: 100px;
+    width: 900px;
+    margin-top: 20px;
+    margin-left: 50px;
+    border-radius: 5px;
+`;
+
+const TeamLogo = styled.div`
+    background-color: #FFFFFF;
+    height: 80px;
+    width: 80px;
+    margin-left: 50px;
+    border-radius: 5px;
+`;
+
+const SeatInfo = styled.div`
+    display: flex;
+    flex-direction: column;
+    height: 80px;
+    width: 300px;
+    margin-left: 40px;
+`;
+
+const StadiumName = styled.div`
+    height: 30px;
+    margin-left: 10px;
+    padding-top: 10px;
+`;
+
+const SeatName = styled.div`
+    height: 30px;
+    margin-left: 10px;
+    padding-top: 10px;
+`;
+
+const MoreBtn = styled.button`
+    height: 80px;
+    width: 50px;
+    margin-left: 150px;
+    border-radius: 5px;
+    background-color: #5090d3;
+    border: 0;
+    cursor: pointer;
+    &:hover {
+        border: 2px solid white;
+    }
 `;
 
 const SearchResult = () => {
     return (
         <SearchResultWrapper>
+            <SearchResultContent>
+                <SearchResultInfo>
+                    <TeamLogo/>
+                    <SeatInfo>
+                        <StadiumName>삼성라이온즈파크</StadiumName>
+                        <SeatName>블루존 3-2구역 5열 21</SeatName>
+                    </SeatInfo>
+                    <Rating name="half-rating-read" defaultValue={2.5} precision={0.5} readOnly size="large" />
+                    <MoreBtn>More</MoreBtn>
+                </SearchResultInfo>
+            </SearchResultContent>
         </SearchResultWrapper>
     );
 }
